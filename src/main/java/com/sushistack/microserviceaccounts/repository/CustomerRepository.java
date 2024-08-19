4739416f7ccd76b8a81658d4f123fcdce284e4b4
sushistack <kjm71712@naver.com>
2024-08-19 13:45:11 +0900

    create accounts for microservice

---
+++ src/main/java/com/sushistack/microserviceaccounts/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.sushistack.microserviceaccounts.repository;
+
+import com.sushistack.microserviceaccounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByMobileNumber(String mobileNumber);
+
+
+}
---
+++ src/test/java/com/sushistack/microserviceaccounts/MicroserviceAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sushistack.microserviceaccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceAccountsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
